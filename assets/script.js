b9baadd4ca0b35895c229bf42d9d737b3e887e9f
Weihon79 <ervan.chin@gmail.com>
2024-12-05 17:45:05 +0100

    ajout de toutes les modification apres avoir créer le repo, pas de checkpoint

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,52 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+const bannerImg = document.querySelector('.banner-img');
+const arrowLeft = document.querySelector('.arrow_left');
+const arrowRight = document.querySelector('.arrow_right');
+const dots = document.querySelectorAll('.dot');
+
+let currentIndex = 0;
+
+function updateDots(index) {
+	dots.forEach((dot, i) => {
+		if (i === index) {
+			dot.classList.add("dot_selected");
+		} else {
+			dot.classList.remove("dot_selected");
+		}
+	});
+}
+
+function updateCarousel(index, direction) {
+	if(currentIndex === -1 && direction === "left") {
+		currentIndex = slides.length -1;
+	} else if (currentIndex === slides.length && direction === "right") {
+		currentIndex = 0;
+	}
+
+	const imagePath = `assets/images/slideshow/${slides[currentIndex].image}`;
+	bannerImg.src = imagePath;
+	bannerImg.alt = `Slide ${currentIndex + 1}`;
+
+	const tagLine = slides[currentIndex].tagLine;
+	document.querySelector("p").innerHTML = tagLine;
+
+	console.log(`Clic sur la flèche ${direction}`);
+}
+
+arrowLeft.addEventListener("click", function () {
+	currentIndex = (currentIndex -1);
+	updateCarousel(currentIndex, "left");
+	updateDots(currentIndex);
+});
+
+arrowRight.addEventListener("click", function () {
+	currentIndex = (currentIndex +1);
+	updateCarousel(currentIndex, "right");
+	updateDots(currentIndex);
+});
+
+
+
