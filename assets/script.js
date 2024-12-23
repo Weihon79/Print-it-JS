e12f331d3ab892da9a3346ba94ebbf03109128be
Weihon79 <ervan.chin@gmail.com>
2024-12-23 09:15:51 +0100

    remove parenthese

--- assets/script.js
+++ assets/script.js
@@ -53,13 +53,13 @@ function updateCarousel(index) {
 }
 
 arrowLeft.addEventListener("click", () => {
-	currentIndex = (currentIndex -1);
+	currentIndex = currentIndex - 1;
 	updateCarousel(currentIndex, "left");
 	updateDots(currentIndex);
 });
 
 arrowRight.addEventListener("click", () => {
-	currentIndex = (currentIndex +1);
+	currentIndex = currentIndex + 1;
 	updateCarousel(currentIndex, "right");
 	updateDots(currentIndex);
 });
